d3dc9f1067a4259c3d92dffcbf6fe6aa8348c476
Aftab1112 <you@example.com>
2024-06-19 17:56:19 +0530

    added favicon and implemented signup logic backend

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,62 @@
+import { connectToMongoDB } from "@/database/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connectToMongoDB();
+
+export const POST = async (request: NextRequest) => {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        {
+          error: "User already exists. Login using email and password",
+        },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    return NextResponse.json(
+      {
+        message: "User created successfully",
+        success: true,
+        savedUser,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          error: error.message,
+        },
+        { status: 500 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          error: "An unknown error occured",
+        },
+        { status: 500 }
+      );
+    }
+  }
+};
