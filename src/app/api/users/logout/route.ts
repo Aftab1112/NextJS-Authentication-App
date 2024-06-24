1907b6236b41778d48a4242c430ab289d27ff898
Aftab1112 <you@example.com>
2024-06-25 01:24:24 +0530

    optimized logout code

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -6,27 +6,24 @@ export const GET = async () => {
       message: "Logged out successfully",
       success: true,
     });
-    response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
     return response;
   } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        {
-          error: error.message,
-        },
-        {
-          status: 500,
-        }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          error: "An unknown error occured",
-        },
-        {
-          status: 500,
-        }
-      );
-    }
+    console.error("Logout Error : ", error);
+
+    return NextResponse.json(
+      {
+        error:
+          error instanceof Error ? error.message : "An unknown error occured",
+      },
+      { status: 500 }
+    );
   }
 };
