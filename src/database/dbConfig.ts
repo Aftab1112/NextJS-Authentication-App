2e54b7530ba01dde805f1d9b996ce52a3056fc8d
Aftab1112 <you@example.com>
2024-06-25 20:20:48 +0530

    optimized mongodb logic

--- src/database/dbConfig.ts
+++ src/database/dbConfig.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connectToMongoDB = async () => {
+export const connectToMongoDB = async (): Promise<void> => {
   try {
     await mongoose.connect(
       process.env.MONGO_URI! || "mongodb://localhost:27017"
