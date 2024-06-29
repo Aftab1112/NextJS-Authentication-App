de513cf44a5859be4f3ae225e663aff9820a8aac
Aftab1112 <you@example.com>
2024-06-29 14:49:47 +0530

    fixed database error

--- src/database/dbConfig.ts
+++ src/database/dbConfig.ts
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToMongoDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI! || "mongodb://localhost:27017"
-    );
+    await mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
 
     connection.on("connected", () => {
